99956a625e0ab13885df179acf829fe922d4dd0e
Jacky <youngok2004@163.com>
2012-09-13 21:30:04 +0800

    rails new

---
+++ house_in_sight/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseInSight::Application.config.secret_token = 'c40b5aea1385c27ec922c4088bb31df4374c9b885f0521dcdd03d80e65d7460708e8e764eb0ded92a42204a6580f46b5acfd42f24e3458202bca194a3d9ce06f'
