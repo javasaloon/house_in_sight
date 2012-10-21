1dd9e31b8db5fb68c2999d8d7e0a3bae9a54b642
Jacky <youngok2004@163.com>
2012-10-21 20:38:00 +0800

    refine

--- house_in_sight/app/models/apartment.rb
+++ house_in_sight/app/models/apartment.rb
@@ -16,6 +16,8 @@ class Apartment
 
   field :floor, :type => Integer
   field :floor_total, :type => Integer
+  field :road, :type => String
+
 
   field :bedroom_count, :type => Integer
   field :livingroom_count, :type => Integer
@@ -33,7 +35,7 @@ class Apartment
   attr_accessible :id, :description, :image_url, :name, :price, :tag_list,
   :floor, :floor_total, :bedroom_count, :livingroom_count, :washroom_count, 
   :age, :build_type, :new_build, :five_years, :only, :district_id, :created,
-  :modified
+  :modified, :road
   
   #acts_as_taggable
   mount_uploader :image_url, ImageUploader
@@ -41,6 +43,38 @@ class Apartment
   def layout
     "#{self.bedroom_count} 室 #{self.livingroom_count} 厅 #{self.washroom_count} 卫"
   end
+  def road_info
+    if self.road
+      "靠近：#{self.road}"
+    else
+      "不靠路"
+    end
+  end
+  def price_info
+    (self.price/10000).round.to_s + " 万" 
+  end
+  def update_info
+    diff = DateTime.current.to_time() - self.modified.to_time()   
+    time = nil
+    day = diff / (3600 * 24)
+    if day > 1
+      time = day.round().to_s + " 天" 
+    else
+      hour = diff / 3600
+        if hour > 1
+          time = hour.round().to_s + " 小时"
+        else
+          minute = diff / 60
+          if minute > 1
+            time = minute.round().to_s + " 分钟"
+          else
+            time = diff.round().to_s + " 秒"
+          end
+        end
+    end
+    time.to_s + "前更新"
+  end
+
   def floor_info
     "#{self.floor} / #{self.floor_total} 楼"
   end
